0d8c5827b2bc109515729382160b3361f0aaabff
ashnikel <ashnikel@gmail.com>
2019-12-14 13:43:29 +0300

    wip

--- fingertips/src/main.rs
+++ fingertips/src/main.rs
@@ -1,3 +1,33 @@
+use std::fs::File;
+use std::io::prelude::*; //for `Read::read_to_string`
+use std::path::PathBuf;
+use std::sync::mpsc::channel;
+use std::sync::mpsc::Receiver;
+use std::thread::spawn;
+use std::thread::JoinHandle;
+use std::io;
+
+fn start_file_reader_thread(
+    documents: Vec<PathBuf>,
+) -> (Receiver<String>, JoinHandle<io::Result<()>>) {
+    let (sender, receiver) = channel();
+
+    let handle = spawn(move || {
+        for filename in documents {
+            let mut f = File::open(filename)?;
+            let mut text = String::new();
+            f.read_to_string(&mut text)?;
+
+            if sender.send(text).is_err() {
+                break;
+            }
+        }
+        Ok(())
+    });
+
+    (receiver, handle)
+}
+
 fn main() {
-    println!("Hello, world!");
+    start_file_reader_thread(documents: Vec<PathBuf>)
 }
